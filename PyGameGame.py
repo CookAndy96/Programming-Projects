30a2ce40d653a71144775587f074ead9df44e4f3
Andrew Cook <awscy1@gmail.com>
2025-03-11 22:57:29 +0000

    Update PyGameGame.py

--- PyGameGame.py
+++ PyGameGame.py
@@ -20,4 +20,3 @@ pygame.Color(255,0,0)   # -> (255,0,0,255) R, G, B, Alpha
 spamRect = pygame.Rect(10,20,200,300)   # Creates a rectangular object that starts in the top left x,y coords at 10,20 with width 200 pixels and height 300 pixels
 
 spamRect.right  # find the xcoord of the right edge
-
