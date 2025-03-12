bcb81d14d39a88e0b9f41a0a24a1a85f6bd68420
Andrew Cook <awscy1@gmail.com>
2025-03-12 23:07:00 +0000

    Update prompt.c

--- C/prompt.c
+++ C/prompt.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <editline.h>
 
 /* Declare a buffer for user input of size 2048 */
 static char input[2048];
@@ -11,14 +14,23 @@ int main(int argc, char** argv) {
 
 	/* In a never ending loop. Keeps the LISP on the screen at all times. */
 	while (1) {
-		/* Output our prompt */
-		fputs("lispy > ", stdout);
+	/* Output our prompt */
+	/* fputs("lispy > ", stdout); */
 
-		/* Read a line of user input of maximum size 2048 */
-		fgets(input, 2048, stdin);
+		/* Output our prompt and get input */
+		char* input = readline("lispy> ")
+
+	/* Read a line of user input of maximum size 2048 */
+	/*fgets(input, 2048, stdin); */
+
+		/* Add input history */
+		add_history(input);
 
 		/* Echo input back to user */
-		printf("No, you're a %s", input);
+		printf("No, you're a %s\n", input);
+
+		/* Free retrieved input */
+		free(input);
 	}
 	return 0;
 }
