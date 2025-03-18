8a15ef91a223cddc903cc02f63d7b576f3c47a23
Andrew Cook <awscy1@gmail.com>
2025-03-18 23:29:34 +0000

    creating prompt
    push from linux subsystem. learnt about adding editline.h in linux and windows systems

---
+++ C/preprocess_prompt.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* If we are compiling on Windows compile these functions. This if conditional is required if you are on windows? */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+/* Otherwise include the editline headers */
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+
+/* Past this point, the code seen in prompt.c is performed */
+
+int main(int argc, char** argv) {
+
+  puts("Lispy Version 0.0.0.0.1");
+  puts("Press Ctrl+c to Exit\n");
+
+  while (1) {
+
+    /* Now in either case readline will be correctly defined */
+    char* input = readline("lispy> ");
+    add_history(input);
+
+    printf("No you're a %s\n", input);
+    free(input);
+
+  }
+
+  return 0;
+}
--- C/prompt_not_working.c
+++ C/prompt_not_working.c
@@ -5,6 +5,7 @@
 #include <editline/history.h>
 
 /* Declare a buffer for user input of size 2048 */
+static char input[2048];
 
 int main(int argc, char** argv) {
 
